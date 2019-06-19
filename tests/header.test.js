91627237d4684bfd121c9c6d3b3b82597ff9596b
Guido Santillan <guidosantillan01@gmail.com>
2019-06-19 11:31:59 -0500

    Test session and session.sig

--- tests/header.test.js
+++ tests/header.test.js
@@ -11,7 +11,7 @@ beforeEach(async () => {
 });
 
 afterEach(async () => {
-  await browser.close();
+  // await browser.close();
 });
 
 test('should expect header has correct text', async () => {
@@ -26,4 +26,28 @@ test('should start oauth flow after clicking login', async () => {
   const url = await page.url();
 
   expect(url).toMatch(/accounts\.google\.com/); // regex expression
+});
+
+test.only('should show logout button when signed in', async () => {
+  const id = '5d097fed76e1ad26b4f17df1';
+
+  const Buffer = require('safe-buffer').Buffer;
+  const sessionObject = {
+    passport: {
+      user: id
+    }
+  };
+
+  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
+    'base64'
+  );
+
+  const Keygrip = require('keygrip');
+  const keys = require('../config/keys');
+  const keygrip = new Keygrip([keys.cookieKey]);
+  const sig = keygrip.sign('session=' + sessionString);
+
+  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session.sig', value: sig });
+  await page.goto('localhost:3000');
 });
