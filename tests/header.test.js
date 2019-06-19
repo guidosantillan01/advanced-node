50c7cdfd3a68ff2c4bcab31150072e6d0302651b
Guido Santillan <guidosantillan01@gmail.com>
2019-06-19 14:48:27 -0500

    Implement sessionFactory and userFactory

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,4 +1,6 @@
 const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/sessionFactory');
+const userFactory = require('./factories/userFactory');
 
 jest.setTimeout(10000); // To fix: Timeout - Async callback was not invoked within the 5000ms timeout specified by jest.setTimeout. problem
 
@@ -31,25 +33,10 @@ test('should start oauth flow after clicking login', async () => {
 });
 
 test('should show logout button when signed in', async () => {
-  const id = '5d097fed76e1ad26b4f17df1';
+  const user = await userFactory();
+  const { session, sig } = sessionFactory(user);
 
-  const Buffer = require('safe-buffer').Buffer;
-  const sessionObject = {
-    passport: {
-      user: id
-    }
-  };
-
-  const sessionString = Buffer.from(JSON.stringify(sessionObject)).toString(
-    'base64'
-  );
-
-  const Keygrip = require('keygrip');
-  const keys = require('../config/keys');
-  const keygrip = new Keygrip([keys.cookieKey]);
-  const sig = keygrip.sign('session=' + sessionString);
-
-  await page.setCookie({ name: 'session', value: sessionString });
+  await page.setCookie({ name: 'session', value: session });
   await page.setCookie({ name: 'session.sig', value: sig });
   await page.goto('localhost:3000');
   await page.waitFor('a[href="/auth/logout"]');
---
+++ tests/setup.js
@@ -0,0 +1,7 @@
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise; // Implements the nodejs global Promise, not the mongoose embedded one.
+mongoose.connect(keys.mongoURI, { useMongoClient: true });
