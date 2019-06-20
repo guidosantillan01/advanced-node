0eae6bce5a43a06f78a57e4c5ecd02738172ecf2
Guido Santillan <guidosantillan01@gmail.com>
2019-06-20 11:26:25 -0500

    User cannot create blog posts test

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -61,3 +61,24 @@ describe('When logged in', async () => {
     });
   });
 });
+
+describe('When user is not logged in', async () => {
+  test('User cannot create blog posts', async () => {
+    const result = await page.evaluate(() => {
+      // Should be a function that returns the fetch statement.
+      return fetch('/api/blogs', {
+        method: 'POST',
+        credentials: 'same-origin',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+          title: 'My Title',
+          content: 'My Content'
+        })
+      }).then(res => res.json());
+    });
+
+    expect(result).toEqual({ error: 'You must log in!' });
+  });
+});
