5d4d94a1234f63060d6059eb037ba3217a93261b
Guido Santillan <guidosantillan01@gmail.com>
2019-06-23 21:55:50 -0500

    Create image upload route

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,3 +1,31 @@
+const AWS = require('aws-sdk');
+const uuid = require('uuid/v1');
+
+const requireLogin = require('../middlewares/requireLogin');
+const keys = require('../config/keys');
+
+const { accessKeyId, secretAccessKey } = keys;
+
+const s3 = new AWS.S3({
+  accessKeyId,
+  secretAccessKey
+});
+
 module.exports = app => {
-  app.get('/api/upload', (req, res) => {});
+  app.get('/api/upload', requireLogin, (req, res) => {
+    // We want the uploaded image to be named like: myUserId/12s3dm39.jpeg
+    const key = `${req.user.id}/${uuid()}.jpeg`;
+
+    s3.getSignedUrl(
+      'putObject',
+      {
+        Bucket: 'my-blog-bucket-guido',
+        ContentType: 'image/jpeg',
+        Key: key
+      },
+      (err, url) => {
+        res.send({ key, url });
+      }
+    );
+  });
 };
